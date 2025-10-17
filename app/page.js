b13d3f75765443dd470460cf89c104752cbe86a2
Roger Garcia <roger.garcia@trueforce.com>
2025-10-17 18:14:14 -0500

    add graphs and shadcn components

--- app/page.js
+++ app/page.js
@@ -1,3 +1,15 @@
+import { BarGraph } from "@/components/charts/bar"
+import { NavBar } from "@/components/general/nav-bar"
+
 export default function Home() {
-  return <div className="font-sans grid ">PULSE AI ADMIN</div>;
+  return (
+    <div className="font-sans min-h-screen flex flex-col">
+      <NavBar />
+      <div className="flex-grow grid place-items-center">
+        <div className="w-[500px] h-[300px]">
+          <BarGraph />
+        </div>
+      </div>
+    </div>
+  );
 }
